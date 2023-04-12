5c22e148665aa33ec21fb632a196611d1680beb2
abhishek Vaghela <abhishek.v@simformsolutions.com>
2023-04-12 14:27:46 +0530

    webpack added

---
+++ config.dev.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/App.js",
+  //   loader
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  // loader
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3500,
+  },
+};
---
+++ src/other.js
@@ -0,0 +1,3 @@
+export default function Other() {
+  return "other function App.js...";
+}
